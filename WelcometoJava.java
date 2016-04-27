9b0cdf194bb4fc4400ff89e3e2bff9e838e976ba
Francisco Sanchez <pacoworld@gmail.com>
2016-04-27 17:35:10 -0500

    Welcome to Java

---
+++ WelcometoJava.java
@@ -0,0 +1,11 @@
+//Francisco Sanchez Enriquez
+
+
+public class Lerning {     
+  
+        public static void main(String[] arguments){         
+ 
+    System.out.println("Hello World.");
+    System.out.println("Hello Java.");
+    }
+  }
