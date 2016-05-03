c67f59429b18bf3e4f060c1d67c87fd9b85884d8
Francisco Sanchez <pacoworld@gmail.com>
2016-05-03 17:32:10 -0500

    Java Stdin and Stdout 1

---
+++ JavaStdinandStdout1.java
@@ -0,0 +1,17 @@
+//Francisco Sanchez Enriquez
+import java.util.Scanner;
+
+public class Lerning {     
+  
+        public static void main(String[] arguments){        
+            
+        Scanner input = new Scanner(System.in);
+       int number1 = input.nextInt();
+       int number2 = input.nextInt();
+       int number3 = input.nextInt();
+       
+       System.out.println(number1);
+       System.out.println(number2);
+       System.out.println(number3);
+  }
+}
