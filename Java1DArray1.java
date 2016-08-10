4ee0af643c706057274328513e1079a958f0873a
Francisco Sanchez <pacoworld@gmail.com>
2016-08-10 16:34:43 -0500

    Java 1D Array

---
+++ Java1DArray1.java
@@ -0,0 +1,24 @@
+//Francisco Sanchez Enriquez  Implementation < Jumping on the Clouds
+import java.util.*;
+
+public class Lerning {
+
+    public static void main(String[] args) {
+         
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int a[] = new int[n];
+
+        for(int i = 0 ; i < n; i++){
+            int val = scan.nextInt();
+            a[i]= val;
+        }
+		
+		scan.close();
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+    }
+}
+    }   
+}
