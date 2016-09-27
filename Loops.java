76e5a6ea8fa123dd70e5418abc73e9fa01b71bce
Francisco Sanchez <pacoworld@gmail.com>
2016-09-26 22:27:37 -0700

    Day 5: Loops

---
+++ Loops.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Lerning{
+    	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+                int n = sc.nextInt();
+                int result;
+                for(int i=1; i <= 10; i++){
+                result = i * n;    
+                System.out.println( n + " x " + i + " = " + result);
+                }
+		
+    }
+}
