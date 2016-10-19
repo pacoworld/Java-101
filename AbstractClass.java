0592b48c90f867dbb7cc6d0c7993084ff9f15642
Francisco Sanchez <pacoworld@gmail.com>
2016-10-19 15:30:47 -0700

    Abstract Class

---
+++ AbstractClass.java
@@ -0,0 +1,30 @@
+//Francisco Sanchez Enriquez    30 Days of Code <  Java Abstract Class
+import java.util.*;
+
+abstract class Book{
+	String title;
+	abstract void setTitle(String s);
+	String getTitle(){
+		return title;
+	}
+}
+
+class MyBook extends Book{        
+    @Override
+    void setTitle(String x){
+       title=x;
+        }  
+    }
+
+class Lerning{ 
+        public static void main(String []args){
+		
+		Scanner sc=new Scanner(System.in);
+		String title=sc.nextLine();
+		MyBook new_novel=new MyBook();
+		new_novel.setTitle(title);
+		System.out.println("The title is: "+new_novel.getTitle());
+      	sc.close();
+        
+    }
+}
